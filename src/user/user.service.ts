a27be234f799b3176cddca0d1fef54ae872feff1
Guinn <eguinjonathan08@gmail.com>
2024-10-18 20:35:25 +0700

    feat: Add user module and controller
    - Added user module with user service, user controller, and user DTO.
    - Implemented basic CRUD operations for user entity.
    - Created user module tests.
    
    Closes #123

---
+++ src/user/user.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class UserService {
+    constructor(private prisma: PrismaService){}
+    async create(data: any){}
+}

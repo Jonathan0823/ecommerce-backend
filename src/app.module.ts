817c2961e240e8704f22cd9c254bd665d50ff1c4
Guinn <eguinjonathan08@gmail.com>
2024-10-22 06:20:24 +0700

    refactor: Update app.module.ts to import and use UserController and ProductsController

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,18 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma.service';
 import { ProductsModule } from './products/products.module';
-import { CategoryController } from './categories/category.controller';
-import { CategoryModule } from './categories/category.module';
 import { CategoriesController } from './categories/categories.controller';
 import { CategoriesService } from './categories/categories.service';
 import { CategoriesModule } from './categories/categories.module';
+import { UserController } from './user/user.controller';
+import { ProductsController } from './products/products.controller';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from './user/user.service';
+import { ProductsService } from './products/products.service';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule, AuthModule, ProductsModule, CategoryModule, CategoriesModule],
-  controllers: [AppController, CategoryController, CategoriesController],
-  providers: [AppService, PrismaService, CategoriesService],
+  imports: [ConfigModule.forRoot(), UserModule, AuthModule, ProductsModule, CategoriesModule],
+  controllers: [AppController, CategoriesController, UserController, ProductsController],
+  providers: [AppService, PrismaService, CategoriesService, JwtService, UserService, ProductsService],
 })
 export class AppModule {}

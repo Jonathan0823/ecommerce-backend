98499001efc2443a070a35e72a4270c2ac9d2e5c
Guinn <eguinjonathan08@gmail.com>
2024-10-22 17:37:37 +0700

    refactor: Add Orders and Carts modules, controllers, and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,10 +14,16 @@ import { ProductsController } from './products/products.controller';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from './user/user.service';
 import { ProductsService } from './products/products.service';
+import { OrdersModule } from './orders/orders.module';
+import { OrdersController } from './orders/orders.controller';
+import { OrdersService } from './orders/orders.service';
+import { CartsService } from './carts/carts.service';
+import { CartsController } from './carts/carts.controller';
+import { CartsModule } from './carts/carts.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule, AuthModule, ProductsModule, CategoriesModule],
-  controllers: [AppController, CategoriesController, UserController, ProductsController],
-  providers: [AppService, PrismaService, CategoriesService, JwtService, UserService, ProductsService],
+  imports: [ConfigModule.forRoot(), UserModule, AuthModule, ProductsModule, CategoriesModule, OrdersModule, CartsModule],
+  controllers: [AppController, CategoriesController, UserController, ProductsController, OrdersController, CartsController],
+  providers: [AppService, PrismaService, CategoriesService, JwtService, UserService, , OrdersService, CartsService],
 })
 export class AppModule {}

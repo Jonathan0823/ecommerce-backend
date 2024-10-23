5317a437d9512c587d9a92fc780ee4055bec51ec
Guinn <eguinjonathan08@gmail.com>
2024-10-23 18:56:10 +0700

    refactor: Simplify CORS configuration in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: process.env.FRONTEND_URL,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+  app.enableCors();
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
